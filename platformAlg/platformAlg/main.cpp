93fa6bb59ff9d1cd04b14310a2af25f15812543f
weili.guo <weili.guo@yintech.cn>
2018-05-25 12:21:54 +0800

    修复内存不释放问题 , 添加读取日志数据函数

--- platformAlg/platformAlg/main.cpp
+++ platformAlg/platformAlg/main.cpp
@@ -4,6 +4,58 @@
 #include <stdio.h>     
 #include <stdlib.h>   
 #include "PlatFormAlgorithm.h"
+#include "Log.h"
+
+
+void ReadKlineData(const char* path, std::map<tagStockCodeInfo, std::vector<tagKline>> & tempMap) {
+	std::ifstream fin(path, std::ios::in);
+	char line[512] = { 0 };
+	tagStockCodeInfo stock(0, "");
+	std::vector<tagKline> tempVector;
+	while (fin.getline(line, sizeof(line))) {
+		char * pp = strchr(line, '-');
+		if(pp){
+			if(!tempVector.empty())
+			{
+				tempMap[stock] = tempVector;
+			}
+			stock.stockcode = pp+1;
+			char buf[20] = {0};
+			memcpy(buf, line, pp - line);
+			stock.market = atoi(buf);
+
+			tempVector.shrink_to_fit();
+			tempVector.clear();
+		}
+		else{
+			const char* token = ",";
+			char * nest_pos;
+			char* pos = strtok_s(line, token, &nest_pos);
+			tagKline temp;
+			int index = 0;
+			while (pos != NULL) {
+				//printf("%s,", pos);
+				if (0 == index)
+					temp.time = _atoi64(pos);
+				else if (1 == index)
+					temp.open = atof(pos);
+				else if (2 == index)
+					temp.high = atof(pos);
+				else if (3 == index)
+					temp.low = atof(pos);
+				else if(4 == index)
+					temp.close = atof(pos);
+				pos = strtok_s(NULL, token, &nest_pos);
+				index++;
+			}
+			
+			tempVector.push_back(temp);
+		}
+		
+	}
+	tempMap[stock] = tempVector;
+	//printf("读取%d行数据,开 高 低 收\n", klineVector.size());
+}
 
 
 void ReadKlineData(const char* path, std::vector<tagKline> & klineVector) {
@@ -38,20 +90,32 @@ void ReadKlineData(const char* path, std::vector<tagKline> & klineVector) {
 
 int main(int argc, char* argv)
 {
-	std::vector<tagKline>  klineVector, klineVector1;
-	ReadKlineData("data.txt", klineVector);
-
-	std::map<tagStockCodeInfo, std::vector<tagKline>>  input;
-	tagStockCodeInfo tagOne((short)111, std::string("601881"));
-
-	input[tagOne] = klineVector;
-	tagStockCodeInfo tagOne1((short)112, std::string("601882"));
-	ReadKlineData("data.txt", klineVector1);
-	input[tagOne1] = klineVector1;
-	std::map<tagStockCodeInfo, tagOutput>  output;
 	CPlatFormAlgorithm plat;
-	plat.select_entrance(input, output, eSinglePlatForm,20,false);
+	{
+		std::map<tagStockCodeInfo, std::vector<tagKline>>  input;
+		ReadKlineData("1526900149_0_log_data.txt", input);
 
-	system("pause");
+		std::map<tagStockCodeInfo, tagOutput>  output;
+		
+		plat.select_entrance(input, output, eSinglePlatForm,15,false);
+	}
+
+	{
+		std::map<tagStockCodeInfo, std::vector<tagKline>>  input;
+		ReadKlineData("1526900215_1_log_data.txt", input);
+		std::map<tagStockCodeInfo, tagOutput>  output;
+
+		plat.select_entrance(input, output, eSinglePlatForm,15,false);
+	}
+
+	{
+		std::map<tagStockCodeInfo, std::vector<tagKline>>  input;
+		ReadKlineData("1526900266_2_log_data.txt", input);
+		std::map<tagStockCodeInfo, tagOutput>  output;
+
+		plat.select_entrance(input, output, eSinglePlatForm,15,false);
+	}
+
+	//system("pause");
 	return 0;
 }
